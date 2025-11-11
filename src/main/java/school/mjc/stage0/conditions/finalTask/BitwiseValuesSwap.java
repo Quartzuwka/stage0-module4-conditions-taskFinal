9b37e94482d64df09f5fe66b64aa740e6da35200
Quartzuwka <kvashnin.semion@yandex.ru>
2025-11-11 16:02:22 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+
+
+        // Swap using XOR
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+
+        System.out.println(first);
+        System.out.println(second);
+
     }
+
+
+
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year <= 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        int days = 2;
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                days = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                days = 30;
+                break;
+            case 2:
+                if (year==2000 || year ==2004)
+                    days = 29;
+                else if (year==2001|| year ==1900)
+                    days = 28;
+                break;
+
+        }
+
+        System.out.println(days);
     }
 }
